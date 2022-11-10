382829c40bea6b308f22bd3e456e90febfa3d460
Diego-Hidalgo <d13g01hl@gmail.com>
2022-11-10 10:28:34 -0500

    edit method contains production with list comprehension

--- src/model/CYK.py
+++ src/model/CYK.py
@@ -8,14 +8,20 @@ class CYK:
         self.__grammar = args[0]
         self.__my_string = args[1]
         self.__initialize_matrix()
-        self.__initial_step()
-        self.__iterate_matrix()
     
     def __initialize_matrix(self):
         n = len(self.__my_string)
         for i in range(0, n):
             self.__matrix.append([])
-
+    
+    def __process_input(self):
+        self.__initial_step()
+        self.__iterate_matrix()
+    
+    def grammar_produces_string(self):
+        self.__process_input()
+        return 'S' in self.__matrix[0][len(self.__my_string)-1]
+    
     @property
     def matrix(self):
         return self.__matrix
@@ -25,11 +31,7 @@ class CYK:
             self.__matrix[i] += [self.__contains_production(self.__my_string[i])]
 
     def __contains_production(self, production):
-        states = []
-        for p in self.__grammar.productions:
-            if production in self.__grammar.productions[p]:
-                states.append(p)
-        return states
+        return [k for k, v in self.__grammar.productions.items() if production in v]
     
     def __check_production(self, B, C, i, j):
         As = list(it.product(B, C))
